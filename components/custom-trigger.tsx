a1693eb8a43b73b6a84e223a5f18b2af0140aabc
yusuf <yusufgad24@gmail.com>
2025-06-09 21:29:58 +0300

    Update CustomTrigger and sidebar components for improved styling and layout
    - Adjusted background and button styles in CustomTrigger for better visual consistency.
    - Changed sidebar width from 17rem to 16rem for a more compact design.

--- components/custom-trigger.tsx
+++ components/custom-trigger.tsx
@@ -9,12 +9,12 @@ export function CustomTrigger() {
 
   return (
     <div
-      className={`bg-sidebar fixed top-2 left-2 z-10 flex items-center gap-1 rounded-lg p-1 ${
+      className={`bg-sidebar/70 fixed top-2 left-2 z-10 flex items-center gap-1 rounded-lg p-1 backdrop-blur-xs ${
         open ? "w-8" : "w-auto"
       }`}
     >
       <Button
-        className="text-sidebar-logo hover:bg-sidebar-border-light bg-sidebar z-20 h-8 w-8 cursor-pointer shadow-none"
+        className="text-sidebar-logo hover:bg-sidebar-border-light z-20 h-8 w-8 cursor-pointer bg-transparent shadow-none"
         onClick={toggleSidebar}
       >
         <PanelLeftIcon />
@@ -22,7 +22,7 @@ export function CustomTrigger() {
       </Button>
 
       <Button
-        className={`text-sidebar-logo hover:bg-sidebar-border-light bg-sidebar h-8 w-8 cursor-pointer shadow-none transition-all duration-200 ease-in-out ${
+        className={`text-sidebar-logo hover:bg-sidebar-border-light h-8 w-8 cursor-pointer bg-transparent shadow-none transition-all duration-200 ease-in-out ${
           open ? "-translate-x-20 opacity-0" : "translate-x-0 opacity-100"
         }`}
       >
@@ -31,7 +31,7 @@ export function CustomTrigger() {
       </Button>
 
       <Button
-        className={`text-sidebar-logo hover:bg-sidebar-border-light bg-sidebar h-8 w-8 cursor-pointer shadow-none transition-all duration-200 ease-in-out ${
+        className={`text-sidebar-logo hover:bg-sidebar-border-light h-8 w-8 cursor-pointer bg-transparent shadow-none transition-all duration-200 ease-in-out ${
           open ? "-translate-x-20 opacity-0" : "translate-x-0 opacity-100"
         }`}
       >
